56ed82b7785479030bf4ed305f6cef8929bfc443
spektruzz <fernando.gonzagaborges@gmail.com>
2025-03-06 00:30:23 -0300

    Adicionando o arquivo classificador.js

---
+++ classificador.js
@@ -0,0 +1,34 @@
+// Declaração de variáveis
+let nome = "Svetlanah";
+let XP = 4500;
+
+//Estrutura de decisão para classificar o herói
+let nivel = "";
+
+if(xp < 1000) {
+    nivel = "Ferro";
+}
+else if(xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+}
+else if(xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+}
+else if(xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+}
+else if(xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+}
+else if(xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+}
+else if(xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+}
+else {  
+    nivel = "Divino";
+}
+
+//Saída
+console.log("O herói de nome ${nome} possui ${xp} está no nível ${nivel}");
